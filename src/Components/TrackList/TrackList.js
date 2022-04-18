d82401ae76ebc46b1cfe0f2f36c1a07576890e0b
zprescott0 <76888154+zprescott0@users.noreply.github.com>
2022-04-18 12:37:06 -0400

    Created TrackList component.

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class TrackList extends React.Component {
+    render() {
+        return (
+            <div className="TrackList">
+                {/* Map method needed here to render a set of Track
+                component.*/}
+            </div>
+        );
+    }
+}
