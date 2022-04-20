56fcb0424f288ded1ff3190873c0ece4bb466104
zprescott0 <76888154+zprescott0@users.noreply.github.com>
2022-04-20 15:45:25 -0400

    Added searchResults to state in app.js.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,6 +7,29 @@ import { Playlist } from '../Playlist/Playlist';
 
 
 class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = { searchResults: [ {
+      TrackName: 'Track Name 1',
+      TrackArtist: 'Track Artist 1',
+      TrackAlbum: 'Track Album 1',
+      id: 1000
+    }, 
+    {
+      TrackName: 'Track Name 2',
+      TrackArtist: 'Track Artist 2',
+      TrackAlbum: 'Track Album 2',
+      id: 1001
+    }, 
+    {
+      TrackName: 'Track Name 3',
+      TrackArtist: 'Track Artist 3',
+      TrackAlbum: 'Track Album 3',
+      id: 1002
+    }
+      ]};
+  }
+
   render() {
     return (
       <div>
