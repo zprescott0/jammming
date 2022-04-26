d26e001bcbf18b0f80bae4fae0040599bd811974
zprescott0 <76888154+zprescott0@users.noreply.github.com>
2022-04-26 11:07:59 -0400

    Set up isRemoval attribute to TrackList component.

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -9,7 +9,8 @@ export class Playlist extends React.Component {
         return (
             <div className="Playlist">
                 <input defaultValue="New Playlist" />
-                <TrackList tracks={this.props.playlistTracks}/>
+                <TrackList tracks={this.props.playlistTracks}
+                isRemoval={true} />
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         );
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -16,7 +16,7 @@ export class TrackList extends React.Component {
             formattedTracks = tracks.map(x => <Track trackName={x.TrackName}
             trackArtist={x.TrackArtist}
             trackAlbum={x.TrackAlbum}
-            isRemoval={false}
+            isRemoval={this.props.isRemoval}
             key={x.id}/>);
         }
 
