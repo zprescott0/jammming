ddeefb1c09d58c462af2bc273113f57fd904986e
zprescott0 <76888154+zprescott0@users.noreply.github.com>
2022-05-01 15:45:22 -0400

    Created new method, handleTermChange, which updates state value searchTerm.

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -8,6 +8,7 @@ export class SearchBar extends React.Component {
         this.state = {searchTerm: 'TestTerm'};
 
         this.search = this.search.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
     }
 
     //********************************************** */
@@ -18,10 +19,17 @@ export class SearchBar extends React.Component {
     }
 
     //********************************************** */
+    //Changes the state of searchTerm.
+    handleTermChange (event) {
+        this.state = { searchTerm: event.target.value };
+    }
+
+    //********************************************** */
     render() {
         return (
             <div className="SearchBar">
-                <input placeholder="Enter a Song, Album, or Artist" />
+                <input placeholder="Enter a Song, Album, or Artist"
+                onChange={this.handleTermChange} />
                 <button className="SearchButton"
                 onClick={this.search}>SEARCH</button>
             </div>
