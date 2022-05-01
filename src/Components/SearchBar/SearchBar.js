2de88ba738ab58ffc8ce1860d3e19e2a6f01d0e0
zprescott0 <76888154+zprescott0@users.noreply.github.com>
2022-05-01 15:33:53 -0400

    Added constructor and search method to SearchBar component.

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,6 +2,22 @@ import React from 'react';
 import './SearchBar.css';
 
 export class SearchBar extends React.Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {searchTerm: 'TestTerm'};
+
+        this.search = this.search.bind(this);
+    }
+
+    //********************************************** */
+    //Calls search method in App.js with the searchTerm in
+    //the SearchBar component.
+    search () {
+        this.props.onSearch(this.state.searchTerm);
+    }
+
+    //********************************************** */
     render() {
         return (
             <div className="SearchBar">
