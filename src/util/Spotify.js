940c2647d2a8d6aa7d69082bc584c5093313b9fc
zprescott0 <76888154+zprescott0@users.noreply.github.com>
2022-05-05 10:46:13 -0400

    Added the ability to capture access token and expire time from URL.

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,4 +1,5 @@
 let userAccessToken = '';
+let expireTime = 0;
 
 const Spotify = {
     getAccessToken: () => {
@@ -7,8 +8,18 @@ const Spotify = {
         }
         else {
             //User Access Token is not set. Authorization needed.
-            const url = window.location.href;
-            console.log(url);
+            const url = new URL(window.location.href);
+
+            const accessTokenRegex = /access_token=([^&]*)/;
+            const expiresInRegex = /expires_in=([^&]*)/;
+
+            const capturedAccessToken = url.href.match(accessTokenRegex);
+            const capturedExpiredValue = url.href.match(expiresInRegex);
+
+            if (capturedAccessToken !== null) {
+                userAccessToken = capturedAccessToken[1];
+                expireTime = capturedExpiredValue[1];
+            }
         }
     }
 };
