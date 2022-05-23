953cd54c63bbe5d4b823f2add80100c4f8fda4b6
zprescott0 <76888154+zprescott0@users.noreply.github.com>
2022-05-23 08:18:32 -0400

    Removed debug button from App.js, removed test values from searchResults state and searchBar component.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -9,30 +9,10 @@ import { Spotify } from '../../util/Spotify';
 class App extends React.Component {
   constructor (props) {
     super(props);
-    this.state = { searchResults: [ {
-      TrackName: 'Track Name 1',
-      TrackArtist: 'Track Artist 1',
-      TrackAlbum: 'Track Album 1',
-      id: 1000,
-      uri: 'qwer'
-    }, 
-    {
-      TrackName: 'Track Name 2',
-      TrackArtist: 'Track Artist 2',
-      TrackAlbum: 'Track Album 2',
-      id: 1001,
-      uri: 'asdf'
-    }, 
-    {
-      TrackName: 'Track Name 3',
-      TrackArtist: 'Track Artist 3',
-      TrackAlbum: 'Track Album 3',
-      id: 1002,
-      uri: 'zxcv'
-    }
-      ],
-    playlistName: 'DEBUG Playlist',
-    playlistTracks: [
+    this.state = { 
+      searchResults: [],
+      playlistName: 'DEBUG Playlist',
+      playlistTracks: [
       {
         TrackName: 'Track Name 4',
         TrackArtist: 'Track Artist 4',
@@ -137,7 +117,6 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <button onClick={Spotify.getAccessToken}>Test</button>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
           <SearchBar onSearch={this.search} />
